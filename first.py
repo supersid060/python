b583dd5ac2fc429306e456f0d166b45187830a98
supersid060 <sabita1279@gmail.com>
2026-09-03 20:11:41 -0500

    first

--- first.py
+++ first.py
@@ -2,5 +2,3 @@ print('hello my name is siddharth')
 print('my hobbies are playing games and doing coding')
 print('the addition of 5+7=',5+7)
 print('the multplication of 6*6=',6*6)
-print('the division of 49/7=',49/7)
-print('the subtraction of 26-10=',26-10),
