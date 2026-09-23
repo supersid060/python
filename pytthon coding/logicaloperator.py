21ec725f85fbc10571f960c8b82de64959a8acfb
supersid060 <sabita1279@gmail.com>
2026-09-22 19:33:09 -0500

    logicaloperators.py

---
+++ pytthon coding/logicaloperator.py
@@ -0,0 +1,18 @@
+a=10
+b=12
+c=12
+print(not(a==b))
+print(not(b==c))
+a="python"
+b="coding"
+if(not(a==b)) :
+    print(a, "and" ,b , "are different")
+a=4
+b=5
+if not (a==1) == (b==5) :
+    print("hello")
+a = int(input("enter a number:"))
+if not (a % 2 == 0) :
+    print("number is odd")
+else :
+    print("number is even")
