1fe547549509b3c8da831aa80a71ab43969d3cb4
supersid060 <sabita1279@gmail.com>
2026-09-22 19:18:50 -0500

    operator.py

---
+++ pytthon coding/operator.py
@@ -0,0 +1,19 @@
+a=12
+b=10
+c=100
+if a and b and c :
+    print("all have boolean value is true")
+else :
+    print("at least one boolean value is false")
+
+a=10
+b=-10
+c=0
+if a>0 or b>0 :
+    print("at least one value is greater than 0")
+else :
+    print("no numbers are greater than 0")
+if b>0 or c>0 :
+    print("at least one value is greater than 0")
+else :
+    print("no numbers are greater than 0")
