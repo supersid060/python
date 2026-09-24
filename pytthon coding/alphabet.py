ba58e0633f2d51276cc74d6b767d849a6f993403
supersid060 <sabita1279@gmail.com>
2026-09-23 19:29:02 -0500

    alphabet.py

---
+++ pytthon coding/alphabet.py
@@ -0,0 +1,6 @@
+charecter="c"
+if charecter == "a" or "b" or "c" or "d" or "e" or "f" or "g" or "h" or "i" or "j" or "k" or "l" or "m" or "n" or "o" or "p" or "q" or "r" or "s" or "t" or "u" or "v" or "w" or "x" or "y" or "z" :
+    print("charecter is in alphabet")
+else :
+    print("it is not in alphabet")
+    
